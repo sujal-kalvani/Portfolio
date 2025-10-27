2c7e4bab49004c7ae60201e0ae15161234370802
sujal-kalvani <sujalkalvani@gmail.com>
2025-10-27 12:26:29 +0530

    save

--- src/app/components/Hexagon.jsx
+++ src/app/components/Hexagon.jsx
@@ -2,8 +2,8 @@
 import Image from "next/image";
 export default function Hexagon() {
     return (
-        <div className="flex justify-center relative max-sm:g">
-            <div className="profile w-[400px] h-[400px] max-md:w-[300px] max-md:h-[300px] max-sm:w-[200px] max-sm:h-[200px] max-sm:mt-10 bg-cyan-500 rounded-full flex overflow-hidden shadow-[0_0_20px_white]"> 
+        <div className="flex justify-center relative ">
+            <div className="profile w-[400px] h-[400px] max-md:w-[300px] max-md:h-[300px] max-sm:w-[200px] max-sm:h-[200px] bg-cyan-500 rounded-full flex overflow-hidden shadow-[0_0_20px_white]"> 
                 <Image
                     className="absolute bottom-[-43px]"
                     src="https://drive.google.com/uc?export=view&id=1pc4dT32RmLILkGoNumYtkBw2ej5k4agW"
